6f208900e62345d6a1519daa9f49ddcecbe02f2b
hieunguyentrong1996 <73066578+hieunguyentrong1996@users.noreply.github.com>
2020-10-28 21:17:48 +0700

    Add project files.

---
+++ CalculateAge/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculateAge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = CalculateAges(DateTime.Now, DateTime.Now.AddYears(89));
+        }
+
+        public static int CalculateAges(DateTime start, DateTime end)
+        {
+            int age = end.Year - start.Year;
+            if (start > end.AddYears(-age))
+                age--;
+            return age;
+        }
+    }
+}
